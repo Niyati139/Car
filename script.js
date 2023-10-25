37ca576e78b8cef410210a6ed2e2c28e322f5f8d
Niyati Parihar <116427211+Niyati139@users.noreply.github.com>
2023-10-25 23:37:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+gsap.to("#one",{
+    delay:0.4,
+    x:-60,
+    repeat:-1,
+    duration:1,
+})
+gsap.to("#two",{
+    delay:0.4,
+        x:60,
+        repeat:-1,
+        duration:1,
+})
+
+gsap.to("#three",{
+    delay:0.4,
+        x:-60, 
+        repeat:-1, 
+        duration:1,
+})
+
